97626cf1ce3e85d861ba7b71fadb24ea0b3302b2
Arnaud Juracek <arnaud.juracek@gmail.com>
2024-10-17 16:41:29 +0200

    Handle PNG not exported from this project

--- firmware/datalight.py
+++ firmware/datalight.py
@@ -21,7 +21,7 @@ LED_COUNT = 300
 LED_PIN = 18          # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 10  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0
 LED_STRIP = ws.SK6812_STRIP_RGBW
@@ -55,26 +55,32 @@ if __name__ == '__main__':
   filename = sys.argv[1]
   image = Image.open(filename)
   pixels = image.load()
-  frameRateSearch = re.search(r'@([\d\.]+)fps', image.text.get('Comment'), re.IGNORECASE)
+  author = image.text.get('Author', 'Unknown')
+  title = image.text.get('Title', 'Untitled')
+  comment = image.text.get('Comment', '')
+  description = image.text.get('Description', '')
+
+  frameRateSearch = re.search(r'@([\d\.]+)fps', comment, re.IGNORECASE)
   if frameRateSearch:
     frameRate = float(frameRateSearch.group(1))
   else:
     warn('No frameRate instruction found, running at 30fps')
     frameRate = 30
 
-
-  print('\n\033[4m' + image.text.get('Title').strip() + '\033[0m,', image.text.get('Author'))
-  print('«', image.text.get('Description').strip(), '»')
+  print('\n\033[4m' + title.strip() + '\033[0m,', author)
+  print('«', description.strip(), '»')
   print(f'{frameRate}fps\n')
 
   for x in range(0, image.width):
     start = time.time()
     for y in range(0, image.height):
-      r, g, b, a = pixels[x, y]
+      r = pixels[x, y][0]
+      g = pixels[x, y][1]
+      b = pixels[x, y][2]
       if (r == g == b):
-        strip.setPixelColor(y, Color(0, 0, 0, r))
+        strip.setPixelColor(image.height - y, Color(0, 0, 0, r))
       else:
-        strip.setPixelColor(y, Color(g, r, b, 0))
+        strip.setPixelColor(image.height - y, Color(g, r, b, 0))
     strip.show()
     print(str(x + 1).rjust(len(str(image.width)), '0'), '/', image.width, '→', '%sms' % int((time.time() - start) * 1000), end='\r')
     time.sleep(1 / frameRate)
